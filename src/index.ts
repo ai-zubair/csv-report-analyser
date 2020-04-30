b3c324434a66d8e04edf1b96ee7cfea970ec92fe
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-05-01 00:15:32 +0530

    Use enums for more readable comparisons

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,35 @@ const matches = fs.readFileSync("football.csv",{
 
 //analyse and report
 let matchesWon = 0;
+
+//constants_mapping
+// const HOME_TEAM     =  1;
+// const AWAY_TEAM     =  2;
+// const MATCH_RESULT  =  5;
+
+// const RESULT = {
+//   HOME_WIN: "H",
+//   AWAY_WIN: "A",
+//   DRAW    : "D",
+// }
+
+//using enums instead
+enum MATCH_STATS {
+  HOME_TEAM     =  1,
+  AWAY_TEAM     =  2,
+  MATCH_RESULT  =  5
+}
+
+enum RESULT{
+  HOME_WIN = "H",
+  AWAY_WIN = "A",
+  DRAW     = "D",
+}
+
 for (const match of matches) {
-  if((match[1]==="Man United" && match[5]==="H")||(match[2]==="Man United" && match[5]==="A")){
+  if((match[MATCH_STATS.HOME_TEAM] === "Man United" && match[MATCH_STATS.MATCH_RESULT] === RESULT.HOME_WIN)||
+     (match[MATCH_STATS.AWAY_TEAM] === "Man United" && match[MATCH_STATS.MATCH_RESULT] === RESULT.AWAY_WIN))
+  {
     matchesWon++;
   }
 }
