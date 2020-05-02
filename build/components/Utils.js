dc45bb05e052e3f139bf7bfadb684470369cdefa
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-05-03 02:33:03 +0530

    Ignore the build directory

--- build/components/Utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function dateStringToDate(dateString) {
-    var dateParts = dateString.split('/').map(function (datePart) { return parseInt(datePart); });
-    return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
-}
-exports.dateStringToDate = dateStringToDate;
