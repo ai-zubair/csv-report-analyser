314dc8b3369b6b0b299c9443a1334cea877f4e9d
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-05-03 15:06:11 +0530

    Add comments for description

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,19 @@ import { Summary } from "./components/Summary";
 import { WinAnalysis } from "./components/Analysers/WinAnalysis";
 import { ConsoleReport } from "./components/Reporters/ConsoleReport";
 
-const genericReader = new CSVfileReader("football.csv");
-const matchReader = new MatchReader(genericReader);
+/* Generically read the data from a csv files into a string[][] */
+const dataReader = new CSVfileReader("football.csv");
+
+/* Read the generic csv string[][] into mathc tuples */
+const matchReader = new MatchReader(dataReader);
 matchReader.load();
 
+/* Create a new analyser */
 const matchAnalyser = new WinAnalysis("Newcastle");
-const reporter = new ConsoleReport();
-const matchSummary = new Summary(matchAnalyser,reporter);
+
+/* Create a new reporter */
+const matchReporter = new ConsoleReport();
+
+/* Create a new summariser */
+const matchSummary = new Summary(matchAnalyser,matchReporter);
 matchSummary.analyseAndReport(matchReader.parsedData);
