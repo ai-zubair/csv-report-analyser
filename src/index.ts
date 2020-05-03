f75068c3072658aeab12f2b02a9bbdbb22c562c3
Zubair Bashir <zubair.bashirzubair@gmail.com>
2020-05-03 17:36:32 +0530

    Remove unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-import { CSVfileReader } from "./components/CSVfileReader";
 import { MatchReader } from "./components/MatchReader";
 import { Summary } from "./components/Summary";
-import { WinAnalysis } from "./components/Analysers/WinAnalysis";
-import { ConsoleReport } from "./components/Reporters/ConsoleReport";
 
 /* Create a match reader pre-configured for CSV files */
 const matchReader = MatchReader.readFromCSV("football.csv");
